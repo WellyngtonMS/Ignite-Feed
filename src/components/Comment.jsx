21ecf0c9daf5223c73ce890618f098f59fca79dd
Wellyngton Matheus <wellyngtoncatarino@gmail.com>
2023-06-21 18:43:58 -0300

    feat: handle like comment functions

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,11 +3,19 @@ import { ThumbsUp } from 'phosphor-react'
 import { Avatar } from './Avatar'
 
 import styles from './Comment.module.css'
+import { useState } from 'react'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
   function handleDeleteComment() {
     onDeleteComment(content)
   }
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
 	return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/WellyngtonMS.png" />
@@ -35,9 +43,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment} title='Aplaudir comentário'>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
